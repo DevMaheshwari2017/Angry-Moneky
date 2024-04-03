b510c90e380e581168b43389adc98f775f35d4ec
Dev <devmaheswari2017@gmail.com>
2024-04-03 18:04:36 +0530

    Made PlayerService non-MonoBehaviour and created a central Service class GameService
    Made PlayerService non-MonoBehaviour and created a central Service class GameService and which inherit from our GenericSingelton Class that we created before and now the GameService is master class which gonna handel all the script communication for every service instead of making every service a singelton like before

---
+++ Assets/Scripts/ServiceLocator/GameService.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ServiceLocator.Utilities;
+using ServiceLocator.Player;
+
+public class GameService : GenericMonoSingleton<GameService>
+{
+    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+
+    public PlayerService playerService{get;private set;}
+
+    private void Start()
+    {
+        playerService = new PlayerService(playerScriptableObject);
+    }
+
+    private void Update()
+    {
+        playerService.Update();
+    }
+
+}
